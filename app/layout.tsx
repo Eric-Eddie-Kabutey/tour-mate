acf1dbe3d80fc959bc8db0228274b0211c2e1fe9
touraye <touraye07@gmail.com>
2026-07-22 12:14:23 +0000

    feat: build stay page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
+import FloatingCategories from "@/components/shared/floating-categories";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <Header />
+        <FloatingCategories /> {/* floating navbar */}
         {children}
         <Footer />
       </body>
---
+++ app/stay/page.tsx
@@ -0,0 +1,32 @@
+import Hero from "@/components/shared/hero";
+import StayFeatures from "@/components/shared/stay-features";
+import NewsletterSignup from "@/components/stay/news-letter-signup";
+import SponsoredFeatures from "@/components/stay/sponsored-feature";
+
+export default function StayPage() {
+    return (<>        
+        <Hero
+            backgroundImage="/assets/images/stay/stays-bg.avif" 
+            subtitle="Luxury hotels, apartments and villas across 20+ African countries."
+            activeCategory="Stays" // Highlights the Stays icon!
+            searchPlaceholder="Where do you want to stay?"
+            searchDropdowns={[ "Property type", "Price", "Amenities" ]}
+        />
+
+        {/* Top Rated Stays */}
+        <StayFeatures />
+
+        {/* Sponsored */}
+        <SponsoredFeatures />
+
+        {/* Recommended stays */}
+        <StayFeatures />
+
+        {/* CTA */}
+        <NewsletterSignup />
+
+        {/* Stays You Might Liked */}
+        <StayFeatures />
+
+    </>)
+}
